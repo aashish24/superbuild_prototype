2d7f4e28674bded37bddda577041521971cf77f3
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-03-16 20:26:14 -0400

    Made improvements

--- CMake/external_paraview.cmake
+++ CMake/external_paraview.cmake
@@ -1,6 +1,7 @@
 set(paraview_source "${lanl_superbuild_build_dir}/paraview")
 set(paraview_binary "${lanl_superbuild_build_dir}/paraview_build")
 set(paraview_install"${lanl_superbuild_install_dir}")
+set(paraview_depends "")
 
 ExternalProject_Add(ParaView
   SOURCE_DIR ${paraview_source}
@@ -11,6 +12,12 @@ ExternalProject_Add(ParaView
   PATCH_COMMAND ""
   CMAKE_ARGS
     ${lanl_superbuild_cmake}
-  INSTALL_COMMAND ""
-  DEPENDS ""
+    -DPARAVIEW_BUILD_QT_GUI:BOOL=OFF
+    -DPARAVIEW_ENABLE_PYTHON:BOOL=ON
+    -DPARAVIEW_USE_MPI:BOOL=ON
+    -DVTK_USE_X:BOOL=OFF
+    -DOSMESA_INCLUDE_DIR:PATH=${OSMESA_INCLUDE_DIR}
+    -DOSMESA_LIBRARY:FILEPATH=${OSMESA_LIBRARY}
+    -DBUILD_FORTRAN_COPROCESSING_ADAPTORS:BOOL=ON
+  DEPENDS ${paraview_depends}
 )
--- CMake/external_subh.cmake
+++ CMake/external_subh.cmake
@@ -1,16 +1,24 @@
 set(subh_source "${lanl_superbuild_build_dir}/subh")
 set(subh_binary "${lanl_superbuild_build_dir}/subh_build")
 set(subh_install"${lanl_superbuild_install_dir}")
+set(subh_depends ParaView)
+
+configure_file("${lanl_cmake_dir}/subh_build_step.cmake.in"
+  "${subh_binary}/subh_build_step.cmake"
+)
+
+set(subh_bulid_command ${CMAKE_COMMAND} -P "${subh_binary}/subh_build_step.cmake")
 
 ExternalProject_Add(subh
   SOURCE_DIR ${subh_source}
   BINARY_DIR ${subh_binary}
   INSTALL_DIR ${subh_install}
-  GIT_REPOSITORY git clone git://github.com/patrickoleary/subh.git
+  GIT_REPOSITORY git://github.com/patrickoleary/subh.git
   GIT_TAG "master"
   PATCH_COMMAND ""
   CMAKE_ARGS
     ${lanl_superbuild_cmake}
-  INSTALL_COMMAND ""
-  DEPENDS ParaView
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${subh_bulid_command}
+  DEPENDS ${subh_depends}
 )
